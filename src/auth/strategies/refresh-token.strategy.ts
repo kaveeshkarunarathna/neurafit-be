45f0c8e51c9159ccb2c31481ed7d7c81475d7d35
kaveeshkarunarathna <129293836+RandimaFdo@users.noreply.github.com>
2026-03-22 10:52:40 +0530

    implement refresh token strategy

---
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { JwtPayload } from './jwt.strategy';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      passReqToCallback: true,
+      secretOrKey: configService.get<string>('jwt.refreshSecret')!,
+    });
+  }
+
+  validate(req: Request, payload: JwtPayload) {
+    const authHeader = req.get('Authorization');
+    const refreshToken = authHeader?.replace('Bearer ', '').trim();
+    return { ...payload, refreshToken };
+  }
+}
