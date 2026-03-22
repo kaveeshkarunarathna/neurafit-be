9c284ee6f547c4a25931449c8947cd6b220c46ee
kaveeshkarunarathna <101452117+kaveeshkarunarathna@users.noreply.github.com>
2026-03-22 14:38:07 +0530

    add current user

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+import { User } from '@prisma/client';
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof User | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user as User;
+
+    return data ? user?.[data] : user;
+  },
+);
