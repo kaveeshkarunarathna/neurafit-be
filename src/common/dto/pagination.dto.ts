63257435b5c94f1998f2d2154262ceb4a213961d
kaveeshkarunarathna <126862511+Dilshan0404@users.noreply.github.com>
2026-03-22 14:38:59 +0530

    Create pagination.dto.ts

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsPositive, Min, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class PaginationDto {
+  @ApiPropertyOptional({ default: 1, minimum: 1 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @ApiPropertyOptional({ default: 10, minimum: 1 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  limit?: number = 10;
+}
